12abfd5e3ecda7b2ecfd6e0bf2287b5faa904901
Vishakh Abhayan <vishakhabhayan0@gmail.com>
2023-03-22 23:00:48 +0530

    display created transactions from database

--- api/index.js
+++ api/index.js
@@ -17,5 +17,11 @@ app.post("/api/transaction", async (req, res) => {
   res.json(transaction);
 });
 
+app.get("/api/transactions", async (req, res) => {
+  await mongoose.connect(process.env.MONGO_URL);
+  const transact = await Transaction.find();
+  res.json(transact);
+});
+
 app.listen(8000);
 //
--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import "./App.css";
-import { initialize } from "workbox-google-analytics";
 
 function App() {
   const [name, setName] = useState("");
   const [dt, setDt] = useState("");
   const [dis, setDis] = useState("");
-  function Addtransactions(ev) {
+  const [transaction, setTransaction] = useState([]);
+
+  useEffect(() => {
+    getTransactions().then(setTransaction);
+  }, []);
+
+  async function getTransactions() {
+    const url = process.env.REACT_APP_API_URL + "/transactions";
+    const response = await fetch(url);
+    return await response.json();
+  }
+
+  function addTransactions(ev) {
     ev.preventDefault(ev);
-    const url = process.env.REACT_APP_API_URL;
+    const url = process.env.REACT_APP_API_URL + "/transaction";
     const price = name.split(" ")[0];
     fetch(url, {
       method: "POST",
@@ -35,7 +46,7 @@ function App() {
         <h1 className="text-4xl font-bold text-fuchsia-100 ">
           ₹20,000 <span>.00</span>
         </h1>
-        <form onSubmit={Addtransactions} className="mt-10">
+        <form onSubmit={addTransactions} className="mt-10">
           <div className="flex gap-1 mb-2">
             <input
               className=" rounded-md py-1 px-3 border-solid border-2 border-rose-200 bg-transparent w-full placeholder-amber-100/50"
@@ -67,19 +78,28 @@ function App() {
             Add new transaction
           </button>
         </form>
-        <div className="mt-3 ">
-          <div className="flex justify-between py-2 px-2 bg-gradient-to-r from-black to-gray-800 rounded-md">
-            <div className="text-left">
-              <div className="text-2xl">New Sumsung TV</div>
-              <div className="text-md text-slate-200">
-                it was time for new tv
+        <div className="mt-3  ">
+          {transaction.length > 0 &&
+            transaction.map((transaction) => (
+              <div className="flex justify-between py-2 px-2 bg-gradient-to-r from-black to-gray-800 rounded-md my-2 shadow-md shadow-slate-500 ">
+                <div className="text-left">
+                  <div className="text-2xl">{transaction.name}</div>
+                  <div className="text-md text-slate-200">
+                    {transaction.dis}
+                  </div>
+                </div>
+                <div className="text-right">
+                  <div
+                    className={
+                      transaction.price < 0 ? "text-red-500" : "text-green-500"
+                    }
+                  >
+                    ₹{transaction.price}
+                  </div>
+                  <div>2022-12-18 15:45</div>
+                </div>
               </div>
-            </div>
-            <div className="text-right">
-              <div className="text-green-300">+ ₹50,000</div>
-              <div>2022-12-18 15:45</div>
-            </div>
-          </div>
+            ))}
         </div>
       </div>
     </div>
